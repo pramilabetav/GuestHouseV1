836469f11fe1859bcce9efc041214d69761c4a89
pramilabetav <39625776+pramilabetav@users.noreply.github.com>
2019-02-11 10:11:08 +0530

    HTML
    HTML Updated

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello Pummy HEllo</h1>
+      <h1>Hello Pummy hghghghgt</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
