0eb025c8b394e096e0e854d72113197d52b5cc08
pramilabetav <39625776+pramilabetav@users.noreply.github.com>
2019-02-12 11:53:27 +0530

    Added other Reducers
    Added other Reducers

---
+++ src/Reducers/RoomReducer.js
@@ -0,0 +1,14 @@
+const RoomReducer = () => {
+  return {
+    RoomData: [
+      {
+        RoomID: "101",
+        FloorName: "Floor1",
+        RoomType: "Shared",
+        ActiveStatus: "A",
+        Capacity: "2"
+      },
+      {}
+    ]
+  };
+};
